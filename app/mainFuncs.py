098ae249a87f14824bc3647ad67d9d5a78921952
jakeberran <jacob.berran@gmail.com>
2023-01-16 12:09:54 -0500

    renamed app to mainFuncs

--- app/mainFuncs.py
+++ app/mainFuncs.py
@@ -5,11 +5,11 @@ from steps.writer.writer import writeEmail
 from steps.sender.sender import sendEmail
 from decouple import config
 from datetime import datetime
-from helpers.db import getDB, updateTopId
+from helpers.db import getDB, overwriteDB, updateTopId
 import logging
 logger = logging.getLogger('app')
 
-def runApp(readNemails = False):
+def readParseAndHandle(readNemails = False):
   logger.info('\n\nSTARTING APP...')
   
   # get in the new emails
@@ -24,13 +24,28 @@ def runApp(readNemails = False):
   # handle the actions by updating the temp database
   handleActions(config('DATABASE_PATH'), actions)
 
+  # update the top id only after we've done everything
+  updateTopId(topId, config('DATABASE_PATH'))
+
+def getCurrentSubRequests():
+  return getDB(config('DATABASE_PATH'))['subRequests']
+
+def updateSubRequests(subRequestsList):
+  topId = getDB(config('DATABASE_PATH'))['lastIdProcessed']
+  dct = {
+    'lastIdProcessed': topId,
+    'subRequests': subRequestsList
+  }
+  overwriteDB(dct, config('DATABASE_PATH'))
+
+def writeAndSend(testing = True):
   # Set variables for writing the email
   From = "Sub Bot"
   test_to = config('TEST_TO_EMAIL')
   real_to = config('REAL_TO_EMAIL')
   
   date_string = datetime.now().strftime("%A, %m/%d (%I:%M %p)")
-  subject = f"\U0001F916 Daily Subs | {date_string}"
+  subject = f"\U0001F916 Daily Subs | {date_string} {'(TESTING)' if testing else ''}"
 
   try:
     if len(getDB(config('DATABASE_PATH'))['subRequests']) == 0:
@@ -38,12 +53,14 @@ def runApp(readNemails = False):
       return
 
     # write the email
-    sendParams = writeEmail(From, test_to, subject, config('DATABASE_PATH'))
+    if testing:
+      to = test_to
+    else:
+      to = real_to
+
+    sendParams = writeEmail(From, to, subject, config('DATABASE_PATH'))
 
     sendEmail(*sendParams)
-
-    # update the top id only after we've done everything
-    updateTopId(topId, config('DATABASE_PATH'))
 
     return
 
@@ -61,4 +78,5 @@ if __name__ == "__main__":
     datefmt='%H:%M:%S'
   )
 
-  runApp()
+  readParseAndHandle()
+  writeAndSend(testing=True)
